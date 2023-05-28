2a09f172e5ed47db683872da5cd1ecf2e0305a75
liankui <yuekewin@gmail.com>
2023-05-28 22:31:33 +0800

    build: add build.sh

---
+++ cmd/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ex
+pwd=$(
+  cd $(dirname $0)
+  pwd
+)
+version=$(cat $(dirname $pwd)/version)
+GOOSs=(darwin linux windows)
+GOARCHs=(amd64)
+
+for os in "${GOOSs[@]}"; do
+  for arch in "${GOARCHs[@]}"; do
+    GOOS=$os GOARCH=$arch go build -ldflags "-w -s" -o openapi-cli-"$version"-"$os"-"$arch"
+  done
+done
+
